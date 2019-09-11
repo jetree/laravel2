fe1ebcbe04f5bacd558af19783bf8ac32aea79c1
神谷友樹 <jetree900@gmail.com>
2019-09-11 23:10:25 +0900

    全てのアクセスでindexを返す

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,8 @@
 |
 */
 
-Route::get('/',function(){
-    return 'hello world';
+Route::get('/{any?}',function(){
+    return view('index');
 });
 // Route::get('/', function () {
 //     return view('welcome');
