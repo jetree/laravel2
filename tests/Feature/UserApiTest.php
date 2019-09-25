7d0fe666de21241c2214ef64e67f971108ef002a
神谷友樹 <jetree900@gmail.com>
2019-09-25 23:03:47 +0900

    ログイン保持のテスト実装

---
+++ tests/Feature/UserApiTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserApiTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp():void
+    {
+        parent::setUp();
+
+        $this->user = factory(user::class)->create();
+    }
+
+    public function test_ログイン中のユーザーを返却する()
+    {
+        $response = $this->actingAs($this->user)->json('GET',route('user'));
+
+        $response->assertStatus(200)->assertJson(['name' => $this->user->name,]);
+        
+    }
+    
+    public function test_ログインされていない場合は空文字を返却する()
+    {
+        $response = $this->json('GET',route('user'));
+        
+        $response->assertStatus(200);
+
+        $this->assertEquals("",$response->content());
+
+    }
+}
